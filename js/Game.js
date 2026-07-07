4be58c979c5320e1ba0887fd4f3b9a906e042153
Firefly10802 <miss.anni2002@mail.ru>
2026-07-07 14:39:33 +0300

    Добавлен мяч, объекты размещены по центру

--- js/Game.js
+++ js/Game.js
@@ -1,10 +1,21 @@
+import Ball from "./entities/Ball.js";
 import Paddle from "./entities/Paddle.js";
+
 
 export default class Game{
     #app;
 
     constructor(app){
         this.#app = app;
-        this.#app.stage.addChild(new Paddle());
+
+        const paddle = new Paddle();
+        paddle.x = 340;
+        paddle.y = 710;
+        this.#app.stage.addChild(paddle);
+
+        const ball = new Ball();
+        ball.x = 400;
+        ball.y = 700;
+        this.#app.stage.addChild(ball);
     }
 }
--- js/entities/Ball.js
+++ js/entities/Ball.js
@@ -1,12 +1,12 @@
 import { Container, Graphics } from "../pixi.mjs";
 
-export default class Paddle extends Container{
+export default class Ball extends Container{
     constructor(){
         super();
 
         const view = new Graphics();
         view.setStrokeStyle({ width: 2, color: 0xff0000});
-        view.rect(0,0,120,60);
+        view.circle(0,0, 10);
         view.stroke();
 
         this.addChild(view);
